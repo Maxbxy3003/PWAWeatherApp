941e6f91721ec0ee00db50dd541d89e8a1f743e4
Maxbxy3003 <2683420@dundee.ac.uk>
2025-11-26 06:44:58 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+//Adds the essential files to cache upon installation
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("weather-cache").then((cache) => {
+            return cache.addAll([
+                "index.html",
+                "style.css",
+                "app.js",
+                "manifest.json",
+                "icon-192.png"
+            ]);
+        })
+    );
+});
+ 
+//Checks cache first, if data not present, check network
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
